fd27182bf9c38319399f0dc522cd5b63de57f970
Abhishek Ravindran <a-10749@ibsplc.com>
2022-08-25 10:59:58 +0530

    [TECH-440][Auth server configuration]

--- src/main/java/com/mystays/authorizationserver/config/WebSecurityConfig.java
+++ src/main/java/com/mystays/authorizationserver/config/WebSecurityConfig.java
@@ -1,26 +1,51 @@
 package com.mystays.authorizationserver.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class WebSecurityConfig {
+@Configuration
+@AllArgsConstructor
+public class WebSecurityConfig  {
 
-    @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+  private final CORSCustomizer corsCustomizer;
 
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate/**")
-                .permitAll().anyRequest().authenticated();
-        return http.build();
-    }
 
-    @Bean
-    protected PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  private static final String LOGIN_PAGE_URL = "http://localhost:3000/logins";
 
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    corsCustomizer.corsCustomizer(http);
+    return http.formLogin()
+            .and()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and().build();
+  }
+
+
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    var u1 = User.withUsername("test").password("12345").authorities("read").build();
+
+    var uds = new InMemoryUserDetailsManager();
+    uds.createUser(u1);
+    return uds;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
 }
