9c239cd53070236d10b8070f3322c23c76865076
Abhishek Ravindran <a-10749@ibsplc.com>
2022-10-12 10:30:54 +0530

    [TECH-468]Custom db changes

--- src/main/java/com/mystays/authorizationserver/scheduler/TokenPurge.java
+++ src/main/java/com/mystays/authorizationserver/scheduler/TokenPurge.java
@@ -1,2 +1,28 @@
-package com.mystays.authorizationserver.scheduler;public class TokenPurge {
+package com.mystays.authorizationserver.scheduler;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.Date;
+
+@Service
+@Transactional
+
+public class TokenPurge {
+
+    final JdbcTemplate jdbcTemplate;
+
+    public TokenPurge(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Scheduled(cron = "${purge.cron.expression}")
+    public void purgeExpired() {
+        Date now = Date.from(Instant.now());
+        System.out.println("Scheduler");
+        jdbcTemplate.update("DELETE FROM mystays.oauth2_authorization WHERE refresh_token_expires_at < NOW()");
+    }
 }
